55318d503b2e1956f3fc2dfa6ee983a866644813
Saleemullahkhan2002 <saleemullahkhan779@gmail.com>
2022-11-27 23:34:07 -0800

    New file ddded

---
+++ main - Copy.dart
@@ -0,0 +1,32 @@
+// void main() {
+//    question no. 1
+//   List<String> mylist = ['bilal', 'bilal', 'bilal', 'owais', 'owais'];
+//   mylist = mylist.toSet().toList();
+//  print(mylist); }
+
+// q no.2
+// // void main() {
+//   List a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+//   print(a);
+
+//   List evenList = [];
+//   List firstOddThenEven = [];
+
+//   for (final i in a) {
+//     if (i.isEven) {
+//       evenList.add(i);
+//     }
+//     {}
+//   }
+//   firstOddThenEven.addAll(evenList);
+//   print(firstOddThenEven);
+// }
+
+// qno.3
+
+void main() {
+  var num = 7;
+  for (var i = 1; i <= 15; i++) {
+    print("$num x $i = ${num * i}");
+  }
+}
